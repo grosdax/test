f30f42942272b352251572abf833adebf6badf3d
grosdax <35136233+grosdax@users.noreply.github.com>
2018-01-05 15:33:52 +0100

    Add files via upload
    ne pas checkin

---
+++ infoPerso.py
@@ -0,0 +1,15 @@
+class infoPerso():
+    def __init__(self):
+        #mail server info
+        self.smpt_server = 'smtp-mail.outlook.com'
+        self.smpt_login ='toto@outlook.fr'
+        self.smpt_pass ='PSWD'
+    
+        #mail server info
+        self.smpt_from = 'toto@outlook.fr'
+        self.smpt_to = 'toto@gmail.com'
+    
+        #gdax autentification info
+        self.gdax_key = 'kkkkkkkkkkkkkkkkkkkkkkkkkkk'
+        self.gdax_b64secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxxxxxxx'
+        self.gdax_passphrase = 'pppppppp'
